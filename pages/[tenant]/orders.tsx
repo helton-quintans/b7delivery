ce94e5d2f21fdba287f6544db16519ba5bc591d4
Helton Quintãns <helton.quit@gmail.com>
2022-05-24 17:48:12 -0300

    :tada: first commit

--- pages/[tenant]/orders.tsx
+++ pages/[tenant]/orders.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -13,9 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Página de pedidos</h1>
 
         <p className={styles.description}>
           Get started by editing{" "}
