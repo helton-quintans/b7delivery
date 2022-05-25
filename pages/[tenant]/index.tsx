e98694fcbeb4eefa4ce30eaa4d6d8a26bfe6600e
Helton Quintãns <helton.quit@gmail.com>
2022-05-24 21:12:38 -0300

    Global styles, Home and SearchInput component

--- pages/[tenant]/index.tsx
+++ pages/[tenant]/index.tsx
@@ -1,5 +1,33 @@
+import { SearchInput } from "../../components/SearchInput";
+import styles from "../../styles/Home.module.css";
+
 const Home = () => {
-  return <div>...</div>;
+  const handleSearch = (searchValue: string) => {
+    console.log(`Você está buscando por ${searchValue}`);
+  };
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.headerTop}>
+          <div className={styles.headerTopLeft}>
+            <div className={styles.headerTitle}>Seja Bem Vindo(a)👋</div>
+            <div className={styles.headerSubtitle}>O que deseja para hoje</div>
+          </div>
+          <div className={styles.headerTopRigth}>
+            <div className={styles.menuButton}>
+              <div className={styles.menuButtonLine}></div>
+              <div className={styles.menuButtonLine}></div>
+              <div className={styles.menuButtonLine}></div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.headerBottom}>
+          <SearchInput mainColor="#fb9400" onSearch={handleSearch} />
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Home;
