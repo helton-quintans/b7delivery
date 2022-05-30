1fba43251c74e1809c83b88ab23b059081100dfa
Helton Quintãns <helton.quit@gmail.com>
2022-05-29 23:35:41 -0300

    feat:swiper autoplay and loop - finish banner

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,5 +1,32 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 import styles from "./style.module.css";
 
 export const Banner = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <Swiper
+        className={styles.swiper}
+        slidesPerView={1}
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay]}
+      >
+        <SwiperSlide className={styles.slide}>
+          <img src="/temp/banner1.png" alt="banner 1" />
+        </SwiperSlide>
+        <SwiperSlide className={styles.slide}>
+          <img src="/temp/banner2.png" alt="banner 2" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
